38f1b1cffc57d190a0ca71166ab34a3db9053f5e
mohitjoshi787898 <mohitjoshi787898@gmail.com>
2024-11-06 12:56:12 +0530

    add contact us and get linkes api

---
+++ routes/getLinksRoute.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const getLinksController = require("../controllers/getLinks");
+
+// GET route to retrieve all links
+
+router.get('/', getLinksController.getLinks);
+
+module.exports = router;
